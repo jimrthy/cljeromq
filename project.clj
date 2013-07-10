ac870fa314785558929225e6f9ad6845988ec8b0
James Ashley <james@teamdaves.us>
2013-07-09 23:31:53 -0500

    Specify that it's my version of cljeromq...although I'm doubtful about the name from the get-go

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljeromq "0.1.0-SNAPSHOT"
+(defproject org.clojars.jimrthy/cljeromq "0.1.0-SNAPSHOT"
   :description "Clojure language wrapper on top of the JNI layer over 0MQ"
   :url "https://github.com/jimrthy/cljeromq"
   :license {:name "Lesser General Public License"
