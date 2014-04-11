d308a0e3ddbb5fa338d5a16740e7726b750d8ff0
James Gatannah <james.gatannah@gmail.com>
2014-04-11 18:57:04 -0500

    Wrap i/o functions in the appropriate macro and be sure they have a ! in the name. Turns out to be almost everything

--- src/cljeromq/core.clj
+++ src/cljeromq/core.clj
@@ -20,8 +20,9 @@
   (:require [byte-transforms :as bt]
             [cljeromq.constants :as K]
             [clojure.edn :as edn]
+            [net.n01se.clojure-jna :as jna]
             [zeromq.zmq :as mq])
-  (:import [org.zeromq ZMQ ZMQ$Context ZMQ$Socket ZMQ$Poller ZMQQueue]
+  (:import [com.sun.jna Pointer Native]
            (java.util Random)
            (java.nio ByteBuffer)))
 
@@ -46,26 +47,27 @@ terminate! on it just before it exits."
   "Stop a messaging context.
 Note that this can have strange effects if there are unclosed sockets."
   [^ZMQ$Context ctx]
-  (.term ctx))
+  (io! (.term ctx)))
 
 (defmacro with-context
-  "Convenience macro for situations where you can create, use, and kill the context in one place."
+  "Convenience macro for situations where you can create, use, and kill the context in one place.
+Seems like a great idea in theory, but doesn't seem all that useful in practice"
   [[id threads] & body]
   `(let [~id (context ~threads)]
      (try ~@body
           (finally (terminate! ~id)))))
 
-(defn socket
+(defn socket!
   "Create a new socket."
   [^ZMQ$Context context type]
   (let [real-type (K/sock->const type)]
-    (.socket context real-type)))
+    (io! (.socket context real-type))))
 
 (defn close!
   "You're done with a socket.
 TODO: Manipulate the socket's linger value appropriately."
   [^ZMQ$Socket s]
-  (.close s))
+  (io! (.close s)))
 
 (defmacro with-socket
   "Convenience macro for handling the start/use/close pattern"
@@ -89,7 +91,7 @@ Any given machine can only have one socket bound to one endpoint at any given ti
 It might be helpful (though ultimately misleading) to think of this call as setting
 up the server side of an interaction."
   [^ZMQ$Socket socket url]
-  (.bind socket url))
+  (io! (.bind socket url)))
 
 (defn bind-random-port!
   "Binds to the first free port. Endpoint should be of the form
@@ -102,20 +104,20 @@ Returns the port!"
   ([^ZMQ$Socket socket endpoint min]
      (bind-random-port! socket endpoint min 65535))
   ([^ZMQ$Socket socket endpoint min max]
-     (.bindToRandomPort socket endpoint min max)))
+     (io! (.bindToRandomPort socket endpoint min max))))
 
 (defn unbind!
   [^ZMQ$Socket socket url]
-  (.unbind socket url))
+  (io! (.unbind socket url)))
 
-(defn bound-socket
+(defn bound-socket!
   "Return a new socket bound to the specified address"
   [ctx type url]
   (let [s (socket ctx type)]
     (bind! s url)
     s))
 
-(defmacro with-bound-socket
+(defmacro with-bound-socket!
   [[name ctx type url] & body]
   (let [name# name]
     `(with-socket [~name# ~ctx ~type]
@@ -127,7 +129,7 @@ Returns the port!"
            ;; going away pretty much immediately.
            (unbind! ~name# ~url))))))
 
-(defmacro with-randomly-bound-socket
+(defmacro with-randomly-bound-socket!
   [[name port-name ctx type url] & body]
   (let [name# name
         port-name# port-name
@@ -137,19 +139,15 @@ Returns the port!"
          (println "DEBUG only: randomly bound port # " ~port-name#)
          (~@body)))))
 
-;; TODO: I don't think this should actually be named w/ a !
-;; Then again, realistically, it *is* all about i/o. So
-;; maybe it should also be contained in an io! block.
 (defn connect!
   [#^ZMQ$Socket socket url]
-  (.connect socket url))
+  (io! (.connect socket url)))
 
-;; Ditto (re: naming convention)
 (defn disconnect!
   [#^ZMQ$Socket socket url]
-  (.disconnect socket url))
+  (io! (.disconnect socket url)))
 
-(defmacro with-connected-socket
+(defmacro with-connected-socket!
   [[name ctx type url] & body]
   (let [name# name
         url# url]
@@ -160,7 +158,7 @@ Returns the port!"
          (finally
            (.disconnect ~name# ~url#))))))
 
-(defn connected-socket
+(defn connected-socket!
   "Returns a new socket connected to the specified URL"
   [ctx type url]
   (let [s (socket ctx type)]
@@ -170,49 +168,49 @@ Returns the port!"
 (defn subscribe!
   ([#^ZMQ$Socket socket #^String topic]
      (doto socket
-       (.subscribe (.getBytes topic))))
+       (io! (.subscribe (.getBytes topic)))))
   ([#^ZMQ$Socket socket]
      (subscribe! socket "")))
 
 (defn unsubscribe!
   ([#^ZMQ$Socket socket #^String topic]
      (doto socket
-       (.unsubscribe (.getBytes topic))))
+       (io! (.unsubscribe (.getBytes topic)))))
   ([#^ZMQ$Socket socket]
      ;; Q: This unsubscribes from everything, doesn't it?
      (unsubscribe! socket "")))
 
 ;;; Send
 
-(defmulti send (fn [#^ZMQ$Socket socket message & flags]
-                 (class message)))
+(defmulti send! (fn [#^ZMQ$Socket socket message & flags]
+                  (class message)))
 
-(defmethod send bytes
-                 ([#^ZMQ$Socket socket #^bytes message flags]
-                    (.send socket message (K/flags->const flags)))
-                 ([#^ZMQ$Socket socket #^bytes message]
-                    (.send socket message (K/flags->const :dont-wait))))
+(defmethod send! bytes
+  ([#^ZMQ$Socket socket #^bytes message flags]
+     (io! (.send socket message (K/flags->const flags))))
+  ([#^ZMQ$Socket socket #^bytes message]
+     (io! (.send socket message (K/flags->const :dont-wait)))))
 
-(defmethod send String
+(defmethod send! String
   ([#^ZMQ$Socket socket #^String message flags]
      ;; FIXME: Debug only
      (println "Sending string:\n" message)
-     (.send #^ZMQ$Socket socket #^bytes (.getBytes message) (K/flags->const flags)))
+     (io! (.send #^ZMQ$Socket socket #^bytes (.getBytes message) (K/flags->const flags))))
   ([#^ZMQ$Socket socket #^String message]
-     (send socket message :dont-wait)))
+     (io! (send socket message :dont-wait))))
 
-(defmethod send Long
+(defmethod send! Long
   ([#^ZMQ$Socket socket #^Long message flags]
   "How on earth is the receiver expected to know the difference
 between this and a String?
 This seems to combine the difficulty that I don't want to be
 handling serialization at this level with the fact that there's
 a lot of annoyingly duplicate boilerplate involved in these."
-     (.send #^ZMQ$Socket socket #^bytes message (K/flags->const flags)))
+  (io! (.send #^ZMQ$Socket socket #^bytes message (K/flags->const flags))))
   ([#^ZMQ$Socket socket #^Long message]
-     (send Long message :dont-wait)))
+     (send! Long message :dont-wait)))
 
-(defmethod send :default
+(defmethod send! :default
   ([#^ZMQ$Socket socket message flags]
      (println "Default Send trying to transmit:\n" message "\n(a"
               (class message) ")")
@@ -222,19 +220,19 @@ a lot of annoyingly duplicate boilerplate involved in these."
      ;; The messaging layer really shouldn't be responsible for
      ;; serialization at all, but it makes sense to at least start
      ;; this out here.
-     (send socket (-> K/const :flag :edn), :send-more)
-     (send socket (str message) flags))
+     (send! socket (-> K/const :flag :edn), :send-more)
+     (send! socket (str message) flags))
   ([#^ZMQ$Socket socket message]
-     (send socket message :dont-wait)))
+     (send! socket message :dont-wait)))
 
-(defn send-partial [#^ZMQ$Socket socket message]
+(defn send-partial! [#^ZMQ$Socket socket message]
   "I'm seeing this as a way to send all the messages in an envelope, except 
 the last.
 Yes, it seems dumb, but it was convenient at one point.
 Honestly, that's probably a clue that this basic idea is just wrong."
-  (send socket message :send-more))
+  (send! socket message :send-more))
 
-(defn send-all [#^ZMQ$Socket socket messages]
+(defn send-all! [#^ZMQ$Socket socket messages]
   "At this point, I'm basically envisioning the usage here as something like HTTP.
 Where the headers back and forth carry more data than the messages.
 This approach is a total cop-out.
@@ -243,18 +241,23 @@ I just need to get something written for my
 \"get the rope thrown across the bridge\" approach.
 It totally falls apart when I'm just trying to send a string."
   (doseq [m messages]
-    (send-partial socket m))
-  (send socket ""))
+    (send-partial! socket m))
+  (send! socket ""))
 
 (defn proxy 
   "Reads from f-in as long as there are messages available,
 forwarding to f-out.
+
+Odds are, this involves i/o so shouldn't happen inside
+a transaction. Who knows, though?
+
 f-out needs to be a function that accepts 1 parameter (whatever
 f-in returned).
 Really nothing more than a convenience function because I've
 found myself writing this pattern a lot, and the messaging functions
 don't seem to lend themselves well to the Seq abstraction...though
 that's really exactly what they're doing.
+
 Q: Why couldn't I handle these messages that way instead? i.e. with
 something like (dorun (map ...))"
   [f-in f-out]
@@ -263,26 +266,26 @@ something like (dorun (map ...))"
       (f-out msg)
       (recur (f-in)))))
 
-(defn identify
+(defn identify!
   [#^ZMQ$Socket socket #^String name]
-  (.setIdentity socket (.getBytes name)))
+  (io! (.setIdentity socket (.getBytes name))))
 
-(defn raw-recv
+(defn raw-recv!
   ([#^ZMQ$Socket socket flags]
      (println "Top of raw-recv")
      (let [flags (K/flags->const flags)]
        (println "Receiving from socket (flags:" flags ")")
-       (.recv socket flags)))
+       (io! (.recv socket flags))))
   ([#^ZMQ$Socket socket]
      (println "Parameterless raw-recv")
-     (raw-recv socket :wait)))
+     (raw-recv! socket :wait)))
 
 (defn bit-array->string [bs]
   ;; Credit:
   ;; http://stackoverflow.com/a/7181711/114334
   (apply str (map #(char (bit-and % 255)) bs)))
 
-(defn recv
+(defn recv!
   "For receiving non-binary messages.
 Strings are the most obvious alternative.
 More importantly (probably) is EDN."
@@ -293,51 +296,52 @@ More importantly (probably) is EDN."
      ;; And then apparently hanging here.
      ;; Well, except that I've successfully set this up to be non-blocking.
      ;; which means I'm getting a nil.
-     (let [binary (raw-recv socket flags)]
-       (println "\tRaw:\n" binary)
-       (let
-           [s (bit-array->string binary)]
-           (println "Received:\n" s)
-           (if (and (.hasReceiveMore socket)
-                    (= s (-> K/const :flag :edn)))
-             (do
-               (println "Should be more pieces on the way")
-               (let [actual-binary (raw-recv socket :dont-wait)
-                     actual-content (bit-array->string actual-binary)]
-                 (println "Actual message:\n" actual-content)
-                 ;; FIXME: Really should loop and build up a sequence.
-                 ;; Absolutely nothing says this will be transmitted one
-                 ;; sequence at a time.
-                 ;; Well, except that doing that is purposefully
-                 ;; difficult.
-                 (edn/read-string actual-content)))
-             s))))
+     (io!
+      (let [binary (raw-recv! socket flags)]
+        (println "\tRaw:\n" binary)
+        (let
+            [s (bit-array->string binary)]
+          (println "Received:\n" s)
+          (if (and (.hasReceiveMore socket)
+                   (= s (-> K/const :flag :edn)))
+            (do
+              (println "Should be more pieces on the way")
+              (let [actual-binary (raw-recv! socket :dont-wait)
+                    actual-content (bit-array->string actual-binary)]
+                (println "Actual message:\n" actual-content)
+                ;; FIXME: Really should loop and build up a sequence.
+                ;; Absolutely nothing says this will be transmitted one
+                ;; sequence at a time.
+                ;; Well, except that doing that is purposefully
+                ;; difficult.
+                (edn/read-string actual-content)))
+            s)))))
   ([#^ZMQ$Socket socket]
-     (recv socket :wait)))
+     (recv! socket :wait)))
 
-(defn recv-more?
+(defn recv-more?!
   [socket]
-  (.hasReceiveMore socket))
+  (io! (.hasReceiveMore socket)))
 
-(defn recv-all
+(defn recv-all!
   "Receive all available message parts.
 Q: Does it make sense to accept flags here?
 A: Absolutely. May want to block or not."
   ([#^ZMQ$Socket socket flags]
       (loop [acc []]
-        (let [msg (recv socket flags)
+        (let [msg (recv! socket flags)
               result (conj acc msg)]
-          (if (recv-more? socket)
+          (if (recv-more?! socket)
             (recur result)
             result))))
   ([#^ZMQ$Socket socket]
      ;; FIXME: Is this actually the flag I want?
-     (recv-all socket :send-more)))
+     (recv-all! socket :wait)))
 
 ;; I strongly suspect these next few methods are the original
 ;; that I've re-written above.
 ;; FIXME: Verify that. See what (if anything) is worth saving.
-(defn recv-str
+(defn recv-str!
   ([#^ZMQ$Socket socket]
       (-> socket recv String. .trim))
   ([#^ZMQ$Socket socket flags]
@@ -346,27 +350,27 @@ A: Absolutely. May want to block or not."
      (when-let [s (recv socket flags)]
        (-> s String. .trim))))
 
-(defn recv-all-str
+(defn recv-all-str!
   "How much overhead gets added by just converting the received primitive
 Byte[] to strings?"
   ([#^ZMQ$Socket socket]
-     (recv-all-str socket 0))
+     (recv-all-str! socket 0))
   ([#^ZMQ$Socket socket flags]
-     (let [packets (recv-all socket flags)]
+     (let [packets (recv-all! socket flags)]
        (map #(String. %) packets))))
 
-(defn recv-obj
+(defn recv-obj!
   "This function is horribly dangerous and really should not be used.
 It's also quite convenient:
 read a string from a socket and convert it to a clojure object.
 That's how this is really meant to be used, if you can trust your peers.
 Could it possibly be used safely through EDN?"
   ([#^ZMQ$Socket socket]
-     (-> socket recv-str read))
+     (-> socket recv-str! read))
   ([#^ZMQ$Socket socket flags]
      ;; This is pathetic, but I'm on the verge of collapsing
      ;; from exhaustion
-     (when-let [s (recv-str socket flags)]
+     (when-let [s (recv-str! socket flags)]
        (read s))))
 
 (defn poller
@@ -376,11 +380,6 @@ Except when they need to.
 There doesn't seem any good reason to put effort into hiding it."
   [socket-count]
   (ZMQ$Poller. socket-count))
-
-;; These next two should be keywords
-(comment
-  (def poll-in ZMQ$Poller/POLLIN)
-  (def poll-out ZMQ$Poller/POLLOUT))
 
 (defmacro with-poller [[poller-name context socket] & body]
   "Cut down on some of the boilerplate around pollers.
@@ -397,7 +396,7 @@ dealing with multiple sockets"
         ctx# context
         s# socket]
     `(let [~name# (mq/poller ~ctx#)]
-       (mq/register ~name# ~s# :pollin :pollerr)
+       (mq/register ~name# ~s# :poll-in :poll-err)
        (try
          ~@body
          (finally
@@ -426,7 +425,7 @@ different."
 (defn register-socket-in-poller!
   "Register a socket to poll on." 
   [#^ZMQ$Socket socket #^ZMQ$Poller poller]
-  (.register poller socket :poll-in))
+  (io! (.register poller socket :poll-in)))
 
 (defn socket-poller-in!
   "Attach a new poller to a seq of sockets.
@@ -437,25 +436,25 @@ Honestly, should be smarter and just let me poll on a single socket."
       (register-socket-in-poller! s checker))
     checker))
 
-(defn dump
+(defn dump!
   "Cheeseball first draft at just logging incoming messages.
 This approach is pretty awful...at the very least it should build
 a string and return that.
 Then again, it's fairly lispy...callers can always rediret STDOUT."
   [#^ZMQ$Socket socket]
   (println (->> "-" repeat (take 38) (apply str)))
-  (doseq [msg (recv-all socket 0)]
+  (doseq [msg (recv-all! socket 0)]
     (print (format "[%03d] " (count msg)))
     (if (and (= 17 (count msg)) (= 0 (first msg)))
       (println (format "UUID %s" (-> msg ByteBuffer/wrap .getLong)))
       (println (-> msg String. .trim)))))
 
-(defn set-id
+(defn set-id!
   ([#^ZMQ$Socket socket #^long n]
     (let [rdn (Random. (System/currentTimeMillis))]
-      (identify socket (str (.nextLong rdn) "-" (.nextLong rdn) n))))
+      (identify! socket (str (.nextLong rdn) "-" (.nextLong rdn) n))))
   ([#^ZMQ$Socket socket]
-     (set-id socket 0)))
+     (set-id! socket 0)))
 
 (defn -main [ & args]
   "This is a library for you to use...if you can figure out how to install it."
