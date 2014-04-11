036dd21d69c9a200ab1364505fc0c9b58f641d8f
James Gatannah <james.gatannah@gmail.com>
2014-04-11 18:59:23 -0500

    First draft at dumping the dependency on the Java JNI bindings. I'm probably setting myself up for failure here, and I know JNA is horribly slow...but JNI just hurts

--- project.clj
+++ project.clj
@@ -1,17 +1,16 @@
-(defproject org.clojars.jimrthy/cljeromq "0.1.0-SNAPSHOT"
-  :description "Clojure language wrapper on top of the JNI layer over 0MQ"
+(defproject org.clojars.jimrthy/cljeromq "0.1.1-SNAPSHOT"
+  :description "Clojure language wrapper using JNA to access 0MQ"
   :url "https://github.com/jimrthy/cljeromq"
   :license {:name "Lesser General Public License"
             :url "http://www.gnu.org/licenses/lgpl.html"}
-  :dependencies [[byte-transforms "0.1.0"]
-                 [com.taoensso/timbre "2.6.2"]
+  :dependencies [[byte-transforms "0.1.2"]
+                 [com.taoensso/timbre "3.1.6"]
                  [net.n01se/clojure-jna "1.0.0"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.zeromq/jzmq4 "4.0.0-SNAPSHOT"]]
+                 [org.clojure/clojure "1.6.0"]]
   :plugins [[lein-midje "3.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]
                                   [org.clojure/tools.namespace "0.2.4"]
-                                  [org.clojure/java.classpath "0.2.1"]]
+                                  [org.clojure/java.classpath "0.2.2"]]
                    :source-paths ["dev"]}}
   :repl-options {:init-ns user}
   :repositories {;"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
--- src/cljeromq/constants.clj
+++ src/cljeromq/constants.clj
@@ -1,72 +1,154 @@
 (ns cljeromq.constants
   ;; TODO: Probably shouldn't be relying on an external logger.
   ;; Then again, the alternatives seem worse.
-  (:require [taoensso.timbre :as timbre])
-  (:import [org.zeromq ZMQ]))
+  (:require [taoensso.timbre :as timbre]))
+
+;;; Basically just copy/pasting a bunch ofmagic numbers, then
+;;; putting them into dictionaries to make them a little easier
+;;; (IMO) to cope with
 
 (def const {
+            ;;; Socket types
+            :socket-type {
+                          ;; Internal 1:1
+                          :pair 0
+
+                          ;; Publish/Subscribe
+                          :pub 1
+                          :sub 2
+                          
+                          ;; Request/Reply
+                          :req 3
+                          :rep 4
+                                    
+                          ;; Router/Dealer
+
+                          ;; Combined ventilator/sink.
+                          ;; Does load balancing on output and fair-queuing on input.
+                          ;; Can shuffle messages out to N nodes then shuffle the replies back.
+                          ;; Raw bidirectional async pattern.
+                          :dealer 5
+
+                          ;; Creates/consumes request-reply routing envelopes.
+                          ;; Lets you route messages to specific connections if you
+                          ;; know their identities.
+                          :router 6
+
+                          ;; Extended Publish/Subscribe
+                          ;; Push/Pull
+                          :pull 7
+                          :push 8
+                                    
+                          :x-pub 9
+                          :x-sub 10
+
+                          :stream 11
+
+                          ;; Obsolete names for Router/Dealer
+                          :xreq 5
+                          :xrep 6}
+
+            :socket-options {:affinity 4
+                             :identity 5
+                             :subscribe 6
+                             :unsubscribe 7
+                             :rate 8
+                             :recovery-interval 9
+                             :send-buffer 11
+                             :receive-buffer 12
+                             :receive-more 13
+                             :fd 14   ;;; ???
+                             :events 15
+                             :type 16
+                             :linger 17
+                             :reconnect-interval 18
+                             :backlog 19
+                             :reconnect-interval-max 21
+                             :max-msg-size 22
+                             :send-hwm 23
+                             :rcv-hwm 24
+                             :multicast-hops 25
+                             :receive-time-out 27
+                             :send-time-out 28                             
+                             :last-endpoint 32
+                             :router-mandatory 33
+                             :tcp-keepalive 34
+                             :tcp-keepalive-count 35
+                             :tcp-keepalive-idle 36
+                             :tcp-keepalive-interval 37
+                             :tcp-accept-filter 38
+                             :immediate 39
+                             :xpub-verbose 40
+                             :router-raw 41
+                             :ipv6 42
+                             :mechanism 43
+                             :plain-server 44
+                             :plain-username 45
+                             :plain-password 46
+                             :curve-server 47  ; 1 for yes, 0 for no
+                             ;; the next two are for the client
+                             :curve-public-key 48
+                             :curve-secret-key 49
+                             ;; According to docs, server just needs private key
+                             :curve-server-key 50
+                             :probe-router 51
+                             :req-correlate 52
+                             :req-relaxed 53
+                             :conflate 54
+                             :zap-domain 55
+                             :router-handover 56
+                             :tos 57  ; ????
+                             :ipc-filter-pid 58
+                             :ipc-filter-uid 59
+                             :ipc-filter-gid 60
+                             :connect-rid 61
+
+                             ;; deprecated
+                             :ipv4-only 31
+                             }
+
             :control {
-                      ;; Non-blocking send/recv
-                      :no-block ZMQ/NOBLOCK
-                      :dont-wait ZMQ/DONTWAIT
+                      ;; Message options
+                      :more 1
+                      :srcfd 2  ; ???
 
                       ;; Blocking (default...doesn't seem to be an 
                       ;; associated named constant)
                       :wait 0
                                 
+                      ;; Non-blocking send/recv
+                      :dont-wait 1
+                      :no-block 1   ; deprecated
+
                       ;; More message parts are coming
-                      :sndmore ZMQ/SNDMORE
-                      :send-more ZMQ/SNDMORE}
+                      :sndmore 2
+                      :send-more 2                      
+                      }
+            :security {:null 0
+                       :plain 1
+                       :curve 2}
             
-            ;;; Socket types
-            :socket-type {
-                          ;; Request/Reply
-                          :req ZMQ/REQ
-                          :rep ZMQ/REP
-                                    
-                          ;; Publish/Subscribe
-                          :pub ZMQ/PUB
-                          :sub ZMQ/SUB
-                          
-                          ;; Extended Publish/Subscribe
-                          :x-pub ZMQ/XPUB
-                          :x-sub ZMQ/XSUB
-                          ;; Push/Pull
-                          
-                          :push ZMQ/PUSH
-                          :pull ZMQ/PULL
-
-                          ;; Internal 1:1
-                          :pair ZMQ/PAIR
-
-                          ;; Router/Dealer
-
-                          ;; Creates/consumes request-reply routing envelopes.
-                          ;; Lets you route messages to specific connections if you
-                          ;; know their identities.
-                          :router ZMQ/ROUTER
-                                    
-                          ;; Combined ventilator/sink.
-                          ;; Does load balancing on output and fair-queuing on input.
-                          ;; Can shuffle messages out to N nodes then shuffle the replies back.
-                          ;; Raw bidirectional async pattern.
-                          :dealer ZMQ/DEALER
-                                    
-                          ;; Obsolete names for Router/Dealer
-                          :xreq ZMQ/XREQ
-                          :xrep ZMQ/XREP}
-
-            ;; TODO: Does the official cljmq language bindings have these
-            ;; defined yet?
-            :socket-options {:curve-server 47  ; 1 for yes, 0 for no
-                             ;; the next two are for the client
-                             :curve-public-key 48
-                             :curve-secret-key 49
-                             ;; According to docs, server just needs private key
-                             :curve-server-key 50}
             ;; Named magical numbers/strings
+            ;; (OK, I made these up)
             :flag
-            {:edn "clojure/edn"}})
+            {:edn "clojure/edn"}
+
+            ;; Note sure what these are actually for.
+            ;; Including for the sake of completeness
+            :transport-events {:connected 1
+                               :delayed 2
+                               :retried 4
+                               :listening 8
+                               :bind-failed 16
+                               :accepted 32
+                               :accept-failed 64
+                               :closed 128
+                               :close-failed 256
+                               :disconnected 512
+                               :monitor-stopped 1024}
+            :multiplex {:poll-in 1
+                        :poll-out 2
+                        :poll-err 4}})
 
 (defn control->const
   "Convert a control keyword to a ZMQ constant"
@@ -93,7 +175,8 @@ socket options."
   (-> :socket-options const key))
 
 (defn version
-  "Return the 0mq version number as a vector"
+  "Return the 0mq version number as a vector
+This doesn't seem to actually belong in here"
   []
   (let [major (ZMQ/getMajorVersion)
         minor (ZMQ/getMinorVersion)
