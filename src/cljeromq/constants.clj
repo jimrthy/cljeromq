fde6ce315da2b7171305ddb418ac55444c29a6a2
James Gatannah <jgatannah@gmail.com>
2015-05-31 22:26:52 +0000

    Whitespace and comments

--- src/cljeromq/constants.clj
+++ src/cljeromq/constants.clj
@@ -12,92 +12,95 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Gory details
 
-(def const {:context-option {
-                             :threads 1  ; default: 1
-                             :max-sockets 2  ; default: 1024
-                             }
-            :control {
-                      ;; Non-blocking send/recv
-                      :no-block ZMQ/NOBLOCK
-                      :dont-wait ZMQ/DONTWAIT
+(def const
+  "TODO: This should be a function instead of a var.
+Although, really, that's almost pedantic."
+  {:context-option {
+                    :threads 1  ; default: 1
+                    :max-sockets 2  ; default: 1024
+                    }
+   :control {
+             ;; Non-blocking send/recv
+             :no-block ZMQ/NOBLOCK
+             :dont-wait ZMQ/DONTWAIT
 
-                      ;; Blocking (default...doesn't seem to be an 
-                      ;; associated named constant)
-                      :wait 0
-                                
-                      ;; More message parts are coming
-                      :sndmore ZMQ/SNDMORE
-                      :send-more ZMQ/SNDMORE}
+             ;; Blocking (default...doesn't seem to be an
+             ;; associated named constant)
+             :wait 0
 
-            :device {:forwarder ZMQ/FORWARDER  ; 2
-                     :queue ZMQ/QUEUE   ; 3
-                     :streamer ZMQ/STREAMER  ; 1
-                     }
+             ;; More message parts are coming
+             :sndmore ZMQ/SNDMORE
+             :send-more ZMQ/SNDMORE}
 
-            :error {:zero 156384712  ; random baseline magic # for 0mq-specific errors
-                    :again 11
-                    :fault 14
-                    :fsm 156384763
-                    :interrupted 4
-                    :not-socket 156384721
-                    :not-supported 156384713
-                    :terminated 156384765}
-            
-            ;; TODO: Access these via iroh?
-            :socket-options {:curve-server 47   ; ZMQ/CURVE_SERVER  ; 1 for yes, 0 for no
-                             ;; the next two are for the client
-                             :curve-public-key 48  ; ZMQ/CURVE_PUBLIC_KEY
-                             :curve-secret-key 49  ; ZMQ/CURVE_SECRET_KEY
-                             ;; The server just needs the private key
-                             ;; The client's what needs this.
-                             :curve-server-key 50  ; ZMQ/CURVE_SERVER_KEY
-                             :identity 5
-                             :receive-more 13
-                             :subscribe 6
-                             :unsubscribe 7
-                             }
+   :device {:forwarder ZMQ/FORWARDER  ; 2
+            :queue ZMQ/QUEUE   ; 3
+            :streamer ZMQ/STREAMER  ; 1
+            }
+
+   :error {:zero 156384712  ; random baseline magic # for 0mq-specific errors
+           :again 11
+           :fault 14
+           :fsm 156384763
+           :interrupted 4
+           :not-socket 156384721
+           :not-supported 156384713
+           :terminated 156384765}
+
+   ;; TODO: Access these via iroh?
+   :socket-options {:curve-server 47   ; ZMQ/CURVE_SERVER  ; 1 for yes, 0 for no
+                    ;; the next two are for the client
+                    :curve-public-key 48  ; ZMQ/CURVE_PUBLIC_KEY
+                    :curve-secret-key 49  ; ZMQ/CURVE_SECRET_KEY
+                    ;; The server just needs the private key
+                    ;; The client's what needs this.
+                    :curve-server-key 50  ; ZMQ/CURVE_SERVER_KEY
+                    :identity 5
+                    :receive-more 13
+                    :subscribe 6
+                    :unsubscribe 7
+                    }
 
             ;;; Socket types
-            :socket-type {
-                          ;; Request/Reply
-                          :req ZMQ/REQ
-                          :rep ZMQ/REP
-                                    
-                          ;; Publish/Subscribe
-                          :pub ZMQ/PUB
-                          :sub ZMQ/SUB
-                          
-                          ;; Extended Publish/Subscribe
-                          :x-pub ZMQ/XPUB
-                          :x-sub ZMQ/XSUB
-                          ;; Push/Pull
-                          
-                          :push ZMQ/PUSH
-                          :pull ZMQ/PULL
+   :socket-type {
+                 ;; Request/Reply
+                 :req ZMQ/REQ
+                 :rep ZMQ/REP
 
-                          ;; Internal 1:1
-                          :pair ZMQ/PAIR
+                 ;; Publish/Subscribe
+                 :pub ZMQ/PUB
+                 :sub ZMQ/SUB
 
-                          ;; Router/Dealer
+                 ;; Extended Publish/Subscribe
+                 :x-pub ZMQ/XPUB
+                 :x-sub ZMQ/XSUB
+                 ;; Push/Pull
 
-                          ;; Creates/consumes request-reply routing envelopes.
-                          ;; Lets you route messages to specific connections if you
-                          ;; know their identities.
-                          :router ZMQ/ROUTER
-                                    
-                          ;; Combined ventilator/sink.
-                          ;; Does load balancing on output and fair-queuing on input.
-                          ;; Can shuffle messages out to N nodes then shuffle the replies back.
-                          ;; Raw bidirectional async pattern.
-                          :dealer ZMQ/DEALER
-                                    
-                          ;; Obsolete names for Router/Dealer
-                          :xreq ZMQ/XREQ
-                          :xrep ZMQ/XREP}
+                 :push ZMQ/PUSH
+                 :pull ZMQ/PULL
 
-            ;; Named magical numbers/strings
-            :flag
-            {:edn "clojure/edn"}})
+                 ;; Internal 1:1
+                 :pair ZMQ/PAIR
+
+                 ;; Router/Dealer
+
+                 ;; Creates/consumes request-reply routing envelopes.
+                 ;; Lets you route messages to specific connections if you
+                 ;; know their identities.
+                 :router ZMQ/ROUTER
+
+                 ;; Combined ventilator/sink.
+                 ;; Does load balancing on output and fair-queuing on input.
+                 ;; Can shuffle messages out to N nodes then shuffle the replies back.
+                 ;; Raw bidirectional async pattern.
+                 :dealer ZMQ/DEALER
+
+                 ;; Obsolete names for Router/Dealer
+                 :xreq ZMQ/XREQ
+                 :xrep ZMQ/XREP}
+
+   ;; Named magical numbers/strings
+   :flag
+   {:edn "clojure/edn"}})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
