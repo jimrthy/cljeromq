9366d5f752ca5414416a3a8ba12474826b8d70ef
James Gatannah <james.gatannah@gmail.com>
2016-08-14 21:03:38 +0000

    Add specs to common schema

--- src/cljeromq/common.clj
+++ src/cljeromq/common.clj
@@ -1,4 +1,5 @@
 (ns cljeromq.common
+  (:require [clojure.spec :as s])
   (:import [org.zeromq
             ZMQ$Context
             ZMQ$Poller
@@ -8,8 +9,21 @@
 ;;; Schema
 
 (def byte-array-type (Class/forName "[B"))
+(s/def ::byte-array-type #(instance? byte-array-type %))
+(s/def ::byte-array-seq (s/coll-of ::byte-array-type))
+
+(comment
+  (let [string->bytes #(->> % (map (comp byte int)) byte-array)
+        frames [(string->bytes "address")
+                (string->bytes "address2")
+                (byte-array [])
+                (string->bytes "body")]]
+    (s/conform ::byte-array-seq frames)))
 
 ;;; Aliases to avoid the ugly java-style nested class names
 (def Context ZMQ$Context)
+(s/def ::Context #(instance? ZMQ$Context %))
 (def Poller ZMQ$Poller)
+(s/def ::Poller #(instance? ZMQ$Poller %))
 (def Socket ZMQ$Socket)
+(s/def ::Socket #(instance? ZMQ$Socket %))
